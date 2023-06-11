4cc913e04b48215f8facbaa43b46c898ec021914
Aadhi22 <77587184+Aadhi22@users.noreply.github.com>
2023-06-11 18:10:17 +0530

    count code commented

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,10 +19,10 @@
     }
   }
 
-  /* number of views using countapi*/
+  /* number of views using countapi
   function callbackName(response) {
     document.getElementById('visits').innerText = response.value;
-}
+}*/
   /**
    * Easy event listener function
    */
